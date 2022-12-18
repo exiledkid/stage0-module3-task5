b7b57cabaf2f477b22c4b05b043d8f95e32ea772
exiledkid <fortunemodspartner@gmail.com>
2022-12-18 17:43:02 +0300

    ...

--- src/main/java/lang/print/gaps/task5/NumberDivider.java
+++ src/main/java/lang/print/gaps/task5/NumberDivider.java
@@ -2,5 +2,9 @@ package lang.print.gaps.task5;
 
 public class NumberDivider {
     void divide(int toBeDivided) {
+
+        double answer = toBeDivided / 5.0;
+
+        System.out.println(answer);
     }
 }
